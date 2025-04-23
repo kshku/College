e860311658153a9bff3e12dc50fa8aa970e99413
kshku <1211shree@gmail.com>
2025-04-23 15:40:35 +0530

    AI lab experiments

---
+++ ai/astar.py
@@ -0,0 +1,48 @@
+def AStarAlgo(start, goal, graph, hueristic):
+    h = hueristic
+    open_list = [(start, [start], h[start])]
+
+    while open_list:
+        print(f"open_list: {open_list}")
+        node, path, nf = open_list[0]
+
+        # Find node with least f(n)
+        for (v, p, f) in open_list:
+            if f < nf:
+                node, path, nf = v, p, f
+
+
+        # Goal check
+        if node == goal:
+            return (path, nf)
+
+        open_list.remove((node, path, nf))
+        print("Popped:", (node, path, nf))
+
+        print(f"Neighbours of {node}: {graph.get(node, [])}")
+
+        for (v, w) in graph.get(node, []):
+            open_list.append((v, path + [v], (nf - h[node]) + w + h[v]))
+
+        print()
+
+    return None
+
+if __name__ == '__main__':
+    graph = {
+        'A': [('B', 4), ('C', 3)],
+        'B': [('F', 5), ('E', 12)],
+        'C': [('E', 10), ('D', 7)],
+        'D': [('C', 7), ('E', 2)],
+        'E': [('B', 12), ('Z', 5)],
+        'F': [('B', 5), ('Z', 16)]
+    }
+
+    hueristic = {'A': 14, 'B': 12, 'C': 11, 'D': 6, 'E': 4, 'F': 11, 'Z': 0}
+
+    path, cost = AStarAlgo('A', 'Z', graph, hueristic)
+
+    if not path:
+        print("No solution")
+    else:
+        print(f"Path: {path}\nCost: {cost}")
---
+++ ai/tic-tac-toe.py
@@ -0,0 +1,63 @@
+import os
+import time
+
+class TicTacToe:
+    def __init__(self):
+        self.state = [' '] * 9
+        self.player = 1
+        self.symbols = ['O', 'X']
+
+    def __str__(self):
+        return f" {self.state[0]} | {self.state[1]} | {self.state[2]}\n___|___|___\n {self.state[3]} | {self.state[4]} | {self.state[5]}\n___|___|___\n {self.state[6]} | {self.state[7]} | {self.state[8]}\n   |   |"
+
+    def checkState(self):
+        for i in range(3):
+            # Check rows
+            if self.state[i * 3] == self.state[i * 3 + 1] == self.state[i * 3 + 2]:
+                return self.state[i * 3]
+            # Check cols
+            if self.state[i] == self.state[i + 3] == self.state[i + 6]:
+                return self.state[i]
+
+        if (self.state[0] == self.state[4] == self.state[8]) or (self.state[2] == self.state[4] == self.state[6]):
+            return self.state[4]
+
+        # Draw
+        if ' ' not in self.state:
+            return None
+
+        return ' '
+
+    def mark(self, pos):
+        if self.state[pos] != ' ':
+            return False
+
+        self.state[pos] = self.symbols[self.player % 2]
+        self.player += 1
+        return True
+
+if __name__ == '__main__':
+    print("Loading the game...")
+    game = TicTacToe()
+    time.sleep(1)
+    running = True
+    result = ' '
+
+    while running:
+        print("Player1 [X] and Player2 [O]\n")
+        print(f"Player{2 if game.player % 2 == 0 else 1} to play")
+        print(game)
+        pos = int(input(f"Enter position to mark [1-9]: "))
+        if not game.mark(pos - 1):
+            print("Invalid position!")
+            time.sleep(1)
+        os.system('clear') # if windows use os.system('cls')
+        result = game.checkState()
+        running = result == ' '
+
+    if not result:
+        print("Draw!")
+    elif result == 'X':
+        print("Player1 won!")
+    else:
+        print("Player2 won!")
